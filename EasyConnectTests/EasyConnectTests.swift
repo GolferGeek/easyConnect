7a22d5e899367db5c2d210099726b06b66fad668
golfergeek <golfergeet@gmail.com>
2024-12-09 15:42:30 -0600

    Initial Commit

---
+++ EasyConnectTests/EasyConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyConnectTests.swift
+//  EasyConnectTests
+//
+//  Created by Matthew Weber on 12/9/24.
+//
+
+import Testing
+@testable import EasyConnect
+
+struct EasyConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
